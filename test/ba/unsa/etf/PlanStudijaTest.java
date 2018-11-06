bf5608283e224a582e382494da457645284122cf
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-11-06 19:23:16 +0100

    Dodan test folder

---
+++ test/ba/unsa/etf/PlanStudijaTest.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanStudijaTest {
+
+    @org.junit.jupiter.api.Test
+    void getIme() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setIme() {
+    }
+}
