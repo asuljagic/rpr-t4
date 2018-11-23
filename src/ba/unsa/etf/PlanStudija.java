d00166c582e972addd15600a4051412d8e2d15ef
asuljagic <43003728+asuljagic@users.noreply.github.com>
2018-11-23 20:33:41 +0100

    Update PlanStudija.java

--- src/ba/unsa/etf/PlanStudija.java
+++ src/ba/unsa/etf/PlanStudija.java
@@ -1,24 +1,39 @@
 package ba.unsa.etf;
 
+import java.util.ArrayList;
+
 public class PlanStudija {
-    private String ime = "";
-    private Semestar studij;
-    private PlanStudija(){};
 
-    public PlanStudija(String ime, Semestar studij) {
-        this.ime = ime;
-        this.studij = studij;
+	private String ime;
+	private ArrayList<Semestar> semestri;
+	private ArrayList<Predmet> obavezniPredmeti;
+	private ArrayList<Predmet> izborniPredmeti;
+
+	PlanStudija(String ime) {
+	    this.ime = ime;
+	    this.semestri = new ArrayList<Semestar>();
+	    this.obavezniPredmeti = new ArrayList<Predmet>();
+        this.izborniPredmeti = new ArrayList<Predmet>();
     }
 
-    public String getIme() {
-        return ime;
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public void dodajSemestar(Semestar semestar) {
+	    this.semestri.add(semestar);
     }
 
-    public void setIme(String ime) {
-        this.ime = ime;
+    public void dodajObavezniPredmet(Predmet predmet){
+	    this.obavezniPredmeti.add(predmet);
     }
 
-    public String ispisi(){
-        return null;
+    public void dodajIzborniPredmet(Predmet predmet) {
+	    this.izborniPredmeti.add(predmet);
     }
+
 }
